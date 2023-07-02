5f72f14e7baeb8b76349299b5f7311a568d908d9
Tejaswita Wakhure <31151862+TejaswitaW@users.noreply.github.com>
2023-07-02 16:08:20 +0530

    contact page template,views,url done

--- resumeproject/urls.py
+++ resumeproject/urls.py
@@ -19,7 +19,8 @@ from core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.home),
-    path('serv/',include('services.urls')),
+    path('',views.home,name='home'),
+    path('contact/',views.contact,name='contact'),
+    path('serv/',include('services.urls')),   
     path('edu/',include('education.urls'))
 ]
